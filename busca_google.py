f782aca20bfde0fe88cfcd04ffd38cb8cdb299c8
Luid Duarte <luidduarte@yahoo.com.br>
2019-08-23 16:14:57 -0300

    modificação na url de busca

--- busca_google.py
+++ busca_google.py
@@ -1,18 +1,7 @@
 from urllib.request import urlopen as uRec
 from bs4 import BeautifulSoup as BeautifulSoup
 
-class Pesquisa:
-                        #mês-dia-ano4digitos
-    def __init__(self, data_inicio, data_fim, tags):
-        self.data_inicio = data_inicio.split('-')
-        self.data_fim = data_fim.split('-')
-        self.tags = []
-        for tag in tags:
-            self.tags.append(tag)
-
-def pesquisar(data_inicio, data_fim, tags):
-    data_inicio = data_inicio.split('-')
-    data_fim = data_fim.split('-')
+def gera_url_pesquisa(data_inicio, data_fim, tags):
     qtd_tags = len(tags)-1
     string_tags = ''
 
@@ -21,6 +10,7 @@ def pesquisar(data_inicio, data_fim, tags):
         qtd_tags -= 1
     string_tags += tags[0]
 
-    url = 'https://www.google.com/search?q={}&tbs=cdr%3A1%2Ccd_min%3A{}%2F{}%2F{}%2Ccd_max%3A{}%2F{}%2F{}&tbm=nws'.format(string_tags,data_inicio[0],data_inicio[1],data_inicio[2],data_fim[0],data_fim[1],data_fim[2])
-
+    url = 'https://www.google.com/search?tbs=cdr:1,cd_min:{},cd_max:{}&tbm=nws&q={}'.format(data_inicio, data_fim, string_tags)
+    
+    return url
 
