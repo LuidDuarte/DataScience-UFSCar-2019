510215cbfe166dbf5efe89819bc4d48e475bbcf1
hidekisrn <sergionisikava@gmail.com>
2019-09-18 00:03:26 -0300

    funcao retorna artigo, a partir de qualquer site de noticia retorna o texto da noticia

--- busca_google.py
+++ busca_google.py
@@ -11,13 +11,12 @@ def gera_url_pesquisa(data_inicio, data_fim, tags):
     string_tags += tags[0]
 
     url = 'https://www.google.com/search?tbs=cdr:1,cd_min:{},cd_max:{}&tbm=nws&q={}'.format(data_inicio, data_fim, string_tags)
-    
     return url
 
 def retorna_buscas_doc(url):
     headers = requests.utils.default_headers() #Se não mudar o header, não tem permissão para acessar (httpError 403)
     headers.update({
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36 OPR/63.0.3368.43',
     })
     uClient = requests.get(url, headers)
     page_html = uClient.content
@@ -33,11 +32,28 @@ def retorna_buscas_doc(url):
         noticia['Manchete'] = noticia_raw.find('h3', {'class': 'r'}).a.text
         noticia['Link'] = noticia_raw.find('h3', {'class': 'r'}).a['href'][7:].split('&sa')[0] # os links começam com /url?q= antes do https:// no href
         fonte_data = (noticia_raw.find('div', {'class': 'slp'}).span.text).split(' - ')
+        noticia['Trecho'] = noticia_raw.find('div', {'class': 'st'}).text
         noticia['Fonte'] = fonte_data[0]
         noticia['Data'] = fonte_data[1]
         noticias.append(noticia)
+    
+    return noticias
 
-    print(noticias[4])
+def retorna_artigo(url):
+    headers = requests.utils.default_headers() #Se não mudar o header, não tem permissão para acessar (httpError 403)
+    headers.update({
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36',
+    })
+    uClient = requests.get(url, headers)
+    page_html = uClient.content
+    uClient.close()
 
-url = 'https://www.google.com/search?tbs=cdr:1,cd_min:08/01/2019,cd_max:08/02/2019&tbm=nws&q=ufscar'
-retorna_buscas_doc(url)
+    page_soup = soup(page_html, 'html.parser')
+    artigo_raw = page_soup.findAll('p')
+    texto = ''
+    for paragrafo in artigo_raw:
+        [script.extract() for script in paragrafo('script')]
+        texto += paragrafo.text
+
+
+    return texto
