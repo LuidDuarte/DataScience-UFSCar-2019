706e5c40c6070b414d9424391cc1177bdf08fade
Luidduarte <luidduarte@yahoo.com.br>
2019-11-18 22:36:18 -0300

    padronização das datas e adição do dia seguinte

--- busca_google.py
+++ busca_google.py
@@ -3,43 +3,13 @@ from bs4 import BeautifulSoup as soup
 from string import punctuation
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
+from datetime import datetime, timedelta
 
-
-def padroniza_data(data):
-    mes_dia, ano = data.split(', ')
-    mes, dia = mes_dia.split(' ')
-    if mes == 'Jan':
-        mes = '01'
-    elif mes == 'Feb':
-        mes = '02'
-    elif mes == 'Mar':
-        mes = '03'
-    elif mes == 'Apr':
-        mes = '04'
-    elif mes == 'May':
-        mes = '05'
-    elif mes == 'Jun':
-        mes = '06'
-    elif mes == 'Jul':
-        mes = '07'
-    elif mes == 'Aug':
-        mes = '08'
-    elif mes == 'Sep':
-        mes = '09'
-    elif mes == 'Oct':
-        mes = '10'
-    elif mes == 'Nov':
-        mes = '11'
-    elif mes == 'Dec':
-        mes = '12'
-
-    return dia + '/' + mes + '/' + ano
-
-def gera_url_pesquisa(data_inicio, data_fim, tags):
+def gera_url_pesquisa(data, tags):
     qtd_tags = len(tags)-1
     string_tags = ''
-    data_inicio = padroniza_data(data_inicio)
-    data_fim = padroniza_data(data_fim)
+    data_inicio = datetime.strptime(data, '%b %d, %Y').strftime('%d/%m/%Y')
+    data_fim = (datetime.strptime(data, '%b %d, %Y') + timedelta(days=1)).strftime('%d/%m/%Y')
     while(qtd_tags > 0):
         string_tags += tags[qtd_tags] + '+'
         qtd_tags -= 1
@@ -95,15 +65,12 @@ def retorna_vetor_noticias(url):
         noticia = {}
         noticia['Manchete'] = noticia_raw.find('h3', {'class': 'r'}).a.text
         noticia['Link'] = noticia_raw.find('h3', {'class': 'r'}).a['href'][7:].split('&sa')[0] # os links começam com /url?q= antes do https:// no href
-        fonte_data = (noticia_raw.find('div', {'class': 'slp'}).span.text).split(' - ')
-        noticia['Fonte'] = fonte_data[0]
-        noticia['Data'] = fonte_data[1]
         noticia['Palavras_Chaves'] = retorna_palavras_chaves(noticia['Link'])
 
         noticias.append(noticia)
     
     return noticias
 
-def retorna_doc(data_inicio, data_fim, tags):
+def retorna_doc_noticias(data_inicio, tags):
     return retorna_vetor_noticias(
         gera_url_pesquisa(data_inicio, data_fim, tags))
